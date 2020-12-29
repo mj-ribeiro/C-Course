5270c24c033d53b3fdab540eac20490c7796db84
Marcos J Ribeiro <mjribeiro@usp.br>
2020-12-29 11:43:20 -0300

    commit inicial

---
+++ cpp1.cpp
@@ -0,0 +1,11 @@
+// https://www.w3schools.com/cpp/cpp_function_multiple.asp
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+cout << "Olá mundo" << endl;
+	return 0;
+}
+
---
+++ cpp3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n1 =10, n2 = 30; 
+	float res = (float)n1 / n2;
+	cout << res;
+	return 0;
+}
